2809e4828bcbf11e7f6ab71403ef8e0c3deeff59
Kishorè Shanto <tinykishore@gmail.com>
2023-07-30 10:01:55 +0600

    added extra components

---
+++ src/components/GatewayTimeout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import gateway_timeout from '$lib/assets/gateway_timeout.svg';
+</script>
+
+<main class="container mx-auto px-8 md:px-12 pb-20 pt-12">
+    <div class="flex flex-col md:grid md:grid-cols-2 gap-6 lg:grid-cols-4 mt-4">
+        <div class="flex flex-col justify-center align-middle items-center col-span-full row-span-full w-full mx-auto h-full">
+            <img class=" h-[30rem] object-contain" src="{gateway_timeout}" alt=""/>
+        </div>
+
+    </div>
+</main>
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,32 @@
     import Footer from "$components/Footer.svelte";
     import PlantsGrid from "$components/PlantsGrid.svelte";
     import PlantCategory from "$components/PlantCategory.svelte";
+    import Loader from "$components/Loader.svelte";
+    import GatewayTimeout from "$components/GatewayTimeout.svelte";
+
+
+    let key = "";
+    let category = "";
+
+    let plantList;
+    let filteredPlantList;
+    let status = 401;
+
+
 </script>
 
+
 <main>
-    <Navbar/>
-    <PlantCategory/>
-    <PlantsGrid/>
+    <Navbar bind:searchKey={key} />
+    <PlantCategory bind:selectedValue={category} />
+
+    {#if status === 401}
+        <Loader/>
+    {:else if status === 200}
+        <PlantsGrid bind:plantList={filteredPlantList}/>
+    {:else}
+        <GatewayTimeout/>
+    {/if}
+
     <Footer/>
 </main>
