a0a65e0eb86f270381c3732d3539945e15b69311
Maria Muna <umuna201429@bscse.uiu.ac.bd>
2023-08-05 09:15:57 +0600

    cookie removed from store

--- src/stores.ts
+++ src/stores.ts
@@ -1,4 +1,5 @@
 import {writable, type Writable} from "svelte/store";
+import type {Cookie} from "@sveltejs/kit/src/runtime/server/page/types";
 
 export const toast = writable({
     title: '',
@@ -6,7 +7,5 @@ export const toast = writable({
     isOpen: false,
 });
 export const cartArray = writable([]);
-
-export const loginSession = <Writable<Cookie>> writable(undefined);
 
 export const googleInitialized = writable(false);
