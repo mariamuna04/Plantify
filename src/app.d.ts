e3024c9a0c62d3a31b15a073b6639a5931b83e3b
Kishorè Shanto <tinykishore@gmail.com>
2023-08-05 09:33:57 +0600

    Fixed global interfaces

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,27 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-declare global {
-    namespace App {
-        // interface Error {}
-        // interface Locals {}
-        // interface PageData {}
-        // interface Platform {}
-    }
+declare namespace App {
+
 }
 
-export {};
+interface Credentials {
+    email: string
+    password: string
+}
+
+interface UserProperties {
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+}
+
+interface UserSession {
+    id: string,
+    user: User
+}
+
+interface SessionCookie {
+    token: string
+}
+
+type User = UserProperties | undefined | null
+type Cookie = SessionCookie | undefined | null
