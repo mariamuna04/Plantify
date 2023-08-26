70d1bb3d67f50be84f6fe389c09cb4b96eb6f9e1
Kishorè Shanto <tinykishore@gmail.com>
2023-08-26 11:30:39 +0600

    PLANTS from mahi

--- src/routes/plants/[plant]/+page.svelte
+++ src/routes/plants/[plant]/+page.svelte
@@ -1,27 +1,180 @@
-<script lang="ts">
+<script lang="ts" xmlns="http://www.w3.org/1999/html">
     import {onMount} from "svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
+    import {Session} from "../../Session";
+    import Footer from "$lib/components/Footer.svelte";
+    import Review from "./Review.svelte";
 
     export let data;
-    let parsedData = JSON.parse(data.plant);
+    let flower = JSON.parse(data.plant);
 
     onMount(() => {
-        document.title = "Plantify - " + parsedData.name;
+        document.title = "Plantify - " + flower.name;
     });
 
 </script>
 
-<p>{parsedData}</p>
-<p>{parsedData.name}</p>
-<p>{parsedData.species}</p>
-<p>{parsedData.family}</p>
-<p>{parsedData.origin}</p>
-<p>{parsedData.description}</p>
-<p>{parsedData.careInstructions}</p>
-<p>{parsedData.averageHeight}</p>
-<p>{parsedData.sunlightRequirements}</p>
-<p>{parsedData.wateringNeeds}</p>
-<p>{parsedData.soilTypePreferences}</p>
-<p>{parsedData.growthHabit}</p>
-<p>{parsedData.flowerColor}</p>
-<p>{parsedData.leafShape}</p>
-<p>{parsedData.foliageTexture}</p>
+<Navbar/>
+<main>
+    <div class="container mx-auto px-12 md:px-36 mb-24">
+        <div class="grid grid-cols-3 gap-x-12">
+            <div class="flex flex-col gap-2 justify-center col-span-2">
+                <svg class="w-full h-fit">
+                    <text x="0%" y="90%" class="w-full">
+                        {flower.name}
+                    </text>
+                </svg>
+                <div class="flex gap-2 align-middle font-mono font-medium text-zinc-400 animate-fadeIn duration-700">
+                    <h1>{flower.species}</h1>
+                    <h1>&bull;</h1>
+                    <h1>{flower.family}</h1>
+                    <h1>&bull;</h1>
+                    <h1 class="text-green-700/60">{flower.category}</h1>
+                </div>
+
+                <div class="mt-6 flex flex-col mb-10">
+                    <h1 class="text-3xl font-black">
+                        Origin
+                    </h1>
+                    <div class="mt-2">
+                        {#each flower.origin as origin}
+                            <h1 class="text-lg font-medium pl-4"> &dash; {origin}</h1>
+                        {/each}
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-2 mt-6 py-12 gap-2">
+
+                <div class="flex flex-col justify-center bg-emerald-200 w-full text-center
+                    rounded-3xl py-3">
+                    <h1 class="text-green-900 font-black">{flower.averageHeight}</h1>
+                    <hr class="border-2 border-white rounded-full my-2">
+                    <h1 class="text-green-600 font-black">Height</h1>
+                </div>
+
+                <div class="flex flex-col justify-center bg-emerald-200 w-full text-center
+                    rounded-3xl py-3">
+                    <h1 class="text-green-900 font-black">{flower.bloomSeason}</h1>
+                    <hr class="border-2 border-white rounded-full my-2">
+                    <h1 class="text-green-600 font-black">Bloom Season</h1>
+                </div>
+
+                <div class="flex flex-col justify-center bg-emerald-200 w-full text-center
+                    rounded-3xl py-3 col-span-2">
+                    <h1 class="text-green-900 font-black">{flower.sunlightRequirements}</h1>
+                    <hr class="border-2 border-white rounded-full my-2">
+                    <h1 class="text-green-600 font-black">Sunlight Requirements</h1>
+                </div>
+
+                <div class="flex flex-col justify-center bg-emerald-200 w-full text-center
+                    rounded-3xl py-3">
+                    <h1 class="text-green-900 font-black">{flower.fruitType}</h1>
+                    <hr class="border-2 border-white rounded-full my-2">
+                    <h1 class="text-green-600 font-black">Fruit Type</h1>
+                </div>
+
+                <div class="flex flex-col justify-center bg-emerald-200 w-full text-center
+                    rounded-3xl py-3">
+                    <h1 class="text-green-900 font-black">{flower.growthHabit}</h1>
+                    <hr class="border-2 border-white rounded-full my-2">
+                    <h1 class="text-green-600 font-black">Growth Havit</h1>
+                </div>
+
+                <div class="flex flex-col justify-center bg-emerald-200 w-full text-center
+                    rounded-3xl py-3">
+                    <h1 class="text-green-900 font-black">{flower.growthHabit}</h1>
+                    <hr class="border-2 border-white rounded-full my-2">
+                    <h1 class="text-green-600 font-black">Growth Havit</h1>
+                </div>
+            </div>
+
+            <div class="flex flex-col col-span-2">
+                <h1 class="text-3xl font-black">
+                    Description
+                </h1>
+                <div class="mt-2">
+                    <h1 class="text-lg font-medium">{flower.description}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-2">
+            <div class="mt-6 flex flex-col">
+                <h1 class="text-3xl font-black">
+                    Care Instructions
+                </h1>
+                <div class="mt-2">
+                    <h1 class="text-lg font-medium">{flower.careInstructions}</h1>
+                </div>
+            </div>
+        </div>
+        <hr>
+
+        {#if (Session.getToken() === undefined)}
+            <h1 class="text-center text-4xl text-zinc-600 font-bold my-8">
+                Sign in for more information and reviews</h1>
+        {:else}
+            <h1 class="text-3xl font-black  mt-6">
+                Additional Information
+            </h1>
+            <div class="grid grid-cols-4 place-items-center  mt-6">
+                <div class="flex flex-col gap-2 bg-emerald-800 w-full h-full text-center
+                    px-2 py-4">
+                    <h1>{flower.averageHeight}</h1>
+                    <h1>{flower.averageHeight}</h1>
+
+                </div>
+
+                <h1>{flower.averageHeight}</h1>
+                <h1>{flower.averageHeight}</h1>
+                <h1>{flower.averageHeight}</h1>
+                <h1>{flower.averageHeight}</h1>
+                <h1>{flower.averageHeight}</h1>
+
+
+            </div>
+        {/if}
+
+    </div>
+
+    <Review bind:plant_id={flower._id}/>
+
+    <section class="my-20"> </section>
+</main>
+<Footer/>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap');
+
+    svg text {
+        position: absolute;
+        animation: stroke 1.5s forwards;
+        stroke-width: 2;
+        stroke: #54A262;
+        font-size: 4.5rem;
+        font-weight: bold;
+    }
+
+    @keyframes stroke {
+        0% {
+            fill: rgba(84, 162, 98, 0);
+            stroke: rgba(84, 162, 98, 1);
+            stroke-dashoffset: 25%;
+            stroke-dasharray: 0 50%;
+            stroke-width: 2;
+        }
+
+        40% {
+            fill: rgba(84, 162, 98, 0);
+            stroke: rgba(84, 162, 98, 1);
+            stroke-width: 1;
+        }
+        100% {
+            fill: rgba(84, 162, 98, 1);
+            stroke: rgba(84, 162, 98, 1);
+            stroke-dashoffset: -25%;
+            stroke-dasharray: 50% 0;
+            stroke-width: 0;
+        }
+    }
+
+</style>
---
+++ src/routes/plants/[plant]/Review.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import {Session} from "../../Session";
+
+    let review:string = ""
+
+    export let plant_id: string;
+
+    const submit = () => {
+        const email = Session.getEmail();
+        console.log({review, email, plant_id});
+        const response = fetch("/api/plants/SubmitReview", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({review, email, plant_id})
+        });
+    }
+
+</script>
+
+<div class="container mx-auto px-8 md:px-64  pt-12">
+    <div class="flex-col flex gap-6 mt-10 justify-end items-end">
+        <h1 class="w-full text-center font-extrabold text-3xl">Write a review</h1>
+        <textarea rows="5" class="bg-zinc-100 shadow-xl rounded-2xl w-full p-4" placeholder="Write your story here"
+                  bind:value={review}></textarea>
+        <button class="font-bold text-white bg-green-900 py-2 px-4 w-fit rounded-full items-end" on:click={submit}>
+            Submit
+        </button>
+    </div>
+</div>
